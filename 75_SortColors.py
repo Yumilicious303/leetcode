e59f00a4eeebddccfaa7297d5369134893c222b6
Yumilicious303 <56486638+Yumilicious303@users.noreply.github.com>
2024-04-08 22:19:39 -0700

    Added Neetcode SortColors solution.

--- 75_SortColors.py
+++ 75_SortColors.py
@@ -16,8 +16,42 @@ def sortColors(nums):
         count[i] = count[i] - 1
 
     return nums
+
+def sortColors2(nums):
+    colorCount = [0, 0, 0]
+    for n in nums:
+        colorCount[n] += 1
+    
+    color = 0
+    for i in range(len(nums)):
+        while colorCount[color] == 0: 
+            color += 1
+        
+        nums[i] = color
+        colorCount[color] -= 1
+    return nums
+
+def sortColorsNeet(nums):
+    l, r = 0, len(nums) - 1
+    i = 0
+
+    def swap(i, j):
+        tmp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = tmp
+
+    while i <= r:
+        if nums[i] == 0:
+            swap(l, i)
+            l += 1
+        elif nums[i] == 2:
+            swap(i, r)
+            r -= 1
+            i -= 1
+        i += 1
+    return nums
         
 
 
 
-print(sortColors([2,0,2,1,1,0]))
+print(sortColorsNeet([2,0,2,1,1,0]))
